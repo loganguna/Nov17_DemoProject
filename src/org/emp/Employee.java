aea66a7427b6b0df0c6781b2d01d5e4243329bf3
loganguna <logan1995.k@gmail.com>
2022-11-17 16:57:05 +0530

    EmployeeDetails file added

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,36 @@
+package org.emp;
+
+public class Employee {
+	
+	private void empId() {
+		System.out.println("empId");
+	}
+	private void empName() {
+		System.out.println("empName");
+	}
+	private void empDob() {
+		System.out.println("empDob");
+	}
+	private void empPhone() {
+		System.out.println("empPhone");
+	}
+	private void empEmail() {
+		System.out.println("empEmail");
+	}
+	private void empAddress() {
+		System.out.println("empAddress");
+	}
+	
+	public static void main(String[] args) {
+		
+		Employee e = new Employee();
+		e.empId();
+		e.empName();
+		e.empDob();
+		e.empPhone();
+		e.empEmail();
+		e.empAddress();
+	}
+	
+
+}
